d58ef5d8295dbc1af9a05a2279ca04213c525402
GianMP22 <gmp5@nyu.edu>
2022-12-03 16:04:39 -0500

    mk1

---
+++ DyslexicText/medium.js
@@ -0,0 +1,8 @@
+// const dracTxt = document.DOCUMENT_TYPE_NODE("DyslexicText/Dracula- Chapter 1.webloc");
+
+let paragraphs = document.body.getElementsByTagName("p");
+
+for (let i=0; i < paragraphs.length; i++) {
+    console.log(paragraphs[i])
+}
+
