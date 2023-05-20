95027f53c897f63e9b53733395770e152f701769
GianMP22 <gmp5@nyu.edu>
2023-05-20 18:45:03 -0400

    working?

---
+++ G/medium.js
@@ -0,0 +1,11 @@
+
+// const { readdir } = require('./node:fs/promises');
+
+// let path = 'Projects/Batmobile:Batmissile';
+// try {
+//   const files = readdir(path);
+//   for (const file of files)
+//     console.log(file);
+// } catch (err) {
+//   console.error(err);
+// } 
